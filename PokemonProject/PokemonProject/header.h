54934388dd73b4795d096f02f349363da9defb09
Walter Hill <walterh24@gmail.com>
2016-11-29 18:22:09 -0500

    Just Getting Started
    - Added Visual Studio project
    - Created header.h, function.cpp, main.cpp

---
+++ PokemonProject/PokemonProject/header.h
@@ -0,0 +1,18 @@
+/*
+Author: Ben Donahue & Walter Hill
+Class: Intro to Programming(CSI 140-08)
+Assignment: Final Project
+Date Assigned: 11/29/16
+Due Date: 12/9/16
+
+Program Description: Final Project - TBD
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given fully-documented references to the work of others.
+I understand the definition and consequences of plagiarism and acknowledge that the assessor of this assignment may,
+for the purpose of assessing this assignment:- Reproduce this assignment and provide a copy to another member of
+academic staff; and/or - Communicate a copy of this assignment to a plagiarism checking service
+(which may then retain a copy of this assignment on its database for the purpose of future plagiarism checking)
+*/
+
+#pragma once
